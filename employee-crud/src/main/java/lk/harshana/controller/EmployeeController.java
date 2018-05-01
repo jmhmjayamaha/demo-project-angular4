1bb4896e48c1c7dc8874fc44dfc0521f36aed530
jmhmjayamaha <jmhmjayamaha@gmail.com>
2018-05-01 21:36:07 +0530

    cros origin is added

--- employee-crud/src/main/java/lk/harshana/controller/EmployeeController.java
+++ employee-crud/src/main/java/lk/harshana/controller/EmployeeController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +21,7 @@ import lk.harshana.services.EmployeeService;
 
 @RestController
 @RequestMapping(value = "/employee")
+@CrossOrigin
 public class EmployeeController {
 
 	@Autowired
