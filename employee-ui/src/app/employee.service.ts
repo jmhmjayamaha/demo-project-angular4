037b018828b003cbecb649fdcb1247ae2706a6ef
jmhmjayamaha <jmhmjayamaha@gmail.com>
2018-05-03 06:03:03 +0530

    update and delete methods

--- employee-ui/src/app/employee.service.ts
+++ employee-ui/src/app/employee.service.ts
@@ -22,4 +22,17 @@ export class EmployeeService {
     return this._http.post('http://localhost:9090/employee', JSON.stringify(post), options)
       .map(result => this.result = result.json());
   }
+
+  updateEmployee(put: Employee, id) {
+    let headers = new Headers({ 'Content-Type': 'application/json'});
+    let options = new RequestOptions({ headers: headers });
+
+    return this._http.put('http://localhost:9090/employee/' + id, JSON.stringify(put), options)
+      .map(result => this.result = result.json());
+  }
+
+  deleteEmployee(id) {
+    return this._http.delete('http://localhost:9090/employee/' + id)
+      .map(result => this.result = result.json());
+  }
 }
